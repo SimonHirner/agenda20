409298931e39387de9bd9f3534f7ecf85656acc4
hm-rittmeie <f.rittmeier@hm.edu>
2020-06-05 20:08:08 +0200

    AG-26 Eingabe eines Abo Codes fehlende Datei hinzugefügt

---
+++ src/main/java/edu/hm/cs/katz/swt2/agenda/persistence/Registration.java
@@ -0,0 +1,13 @@
+package edu.hm.cs.katz.swt2.agenda.persistence;
+
+public class Registration {
+    private String key;
+    
+    public String getKey() {
+        return key;
+    }
+    
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
