9d649faeff7d582756b1af2a23c54cd7d3c3ec1a
hm-rittmeie <f.rittmeier@hm.edu>
2020-06-06 17:50:26 +0200

    AG-37 Custom Error Landing Page

---
+++ src/main/java/edu/hm/cs/katz/swt2/agenda/mvc/ErrorController.java
@@ -0,0 +1,25 @@
+package edu.hm.cs.katz.swt2.agenda.mvc;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller-Klasse für die Error-Page.
+ *
+ * @author Fabian Rittmeier (mailto: rittmeie@hm.edu)
+ */
+
+@Controller
+public class ErrorController extends AbstractController {
+    
+    
+    /**
+     * Erstellt die Error Seite.
+     */
+    @GetMapping("/error")
+    public String getErrorView(Model model, Authentication auth) {
+        return "error";
+    }
+}
