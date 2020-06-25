0131f3f600be7422f50e16093792b12e3684b4b4
SimonHirner <simon.hirner@hm.edu>
2020-06-25 22:54:23 +0200

    AG-57: HTTPS aktiviert.

--- src/main/java/edu/hm/cs/katz/swt2/agenda/AgendaApplication.java
+++ src/main/java/edu/hm/cs/katz/swt2/agenda/AgendaApplication.java
@@ -1,7 +1,14 @@
 package edu.hm.cs.katz.swt2.agenda;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Spring-Boot-Application. Spring-Boot-Anwendungen starten eine Rahmenanwendung, die selbständig
@@ -16,5 +23,37 @@ public class AgendaApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(AgendaApplication.class, args);
+  }
+  
+  /**
+   * Aktivierung von SSL Traffic und Hinzufügen von HTTP zu HTTPS Umleitung.
+   */
+  @Bean
+  public ServletWebServerFactory servletContainer() {
+    TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+      @Override
+      protected void postProcessContext(Context context) {
+        SecurityConstraint securityConstraint = new SecurityConstraint();
+        securityConstraint.setUserConstraint("CONFIDENTIAL");
+        SecurityCollection collection = new SecurityCollection();
+        collection.addPattern("/*");
+        securityConstraint.addCollection(collection);
+        context.addConstraint(securityConstraint);
+      }
+    };
+    tomcat.addAdditionalTomcatConnectors(getHttpConnector());
+    return tomcat;
+  }
+
+  /**
+   * Umleitung von Anfragen auf Port 8080 zu Port 8443.
+   */
+  private Connector getHttpConnector() {
+    Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+    connector.setScheme("http");
+    connector.setPort(8080);
+    connector.setSecure(false);
+    connector.setRedirectPort(8443);
+    return connector;
   }
 }
